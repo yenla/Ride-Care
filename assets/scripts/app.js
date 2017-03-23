fb7cff1085de8a0b1abd1c4bf79c0feb0782f928
Nico Flicker <Nico@Nicos-MacBook-Air.local>
2017-03-22 22:57:52 -0700

    adding changes to the style component

---
+++ assets/scripts/app.js
@@ -0,0 +1,48 @@
+
+
+
+
+// Click event for sign in
+	btnLogin.addEventListener('click', e => {
+		document.getElementById("#googleMap");
+
+	});
+
+
+
+function initMap() {
+
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 7,
+      center: {lat: 34.052235, lng: -118.243683}
+    });
+    directionsDisplay.setMap(map);
+
+    var onChangeHandler = function() {
+      calculateAndDisplayRoute(directionsService, directionsDisplay);
+    };
+    document.getElementById('start').addEventListener('change', onChangeHandler);
+    document.getElementById('end').addEventListener('change', onChangeHandler);
+  
+
+  function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    directionsService.route({
+      origin: document.getElementById('start').value,
+      destination: document.getElementById('end').value,
+      travelMode: 'TRANSIT'
+    }, function(response, status) {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+      new google.maps.DirectionsRenderer({
+      map: mapObject,
+      panel: document.getElementById('directions'),
+      directions: response
+      });
+    });
+  }
+}
